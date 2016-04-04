42a3839229eff956b7278ba2964ad26086ecb100
Alessandro <ale-remo@hotmail.com>
2016-04-04 02:00:54 -0300

    Removing debugging toasts

--- app/src/main/java/com/example/android/buddybuilding/Input1Goal.java
+++ app/src/main/java/com/example/android/buddybuilding/Input1Goal.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.RadioButton;
-import android.widget.Toast;
 
 public class Input1Goal extends AppCompatActivity {
 
@@ -28,30 +27,22 @@ public class Input1Goal extends AppCompatActivity {
             case R.id.loseWeight:
                 if (checked) {
                     userData.setGoal(User.LOSE_WEIGHT);
-                    Helper.makeToast("Losing", getApplicationContext());
                     break;
                 }
             case R.id.maintainWeight:
                 if (checked)
                 {
                     userData.setGoal(User.MAINTAIN_WEIGHT);
-                    Helper.makeToast("Maintaing", getApplicationContext());
                     break;
                 }
             case R.id.gainWeight:
                 if (checked)
                 {
                     userData.setGoal(User.GAIN_WEIGHT);
-                    Helper.makeToast("Gaining", getApplicationContext());
                     break;
                 }
         }
 
         startActivity(new Intent(this, Input2Activity.class));
-    }
-
-    private void makeToast(String text) {
-        Toast toast = Toast.makeText(getApplicationContext(), text, Toast.LENGTH_SHORT);
-        toast.show();
     }
 }
