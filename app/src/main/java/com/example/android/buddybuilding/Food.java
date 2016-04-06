c52303aa15917aedf7f9fb6f0bd4c87837285e9f
Alessandro <ale-remo@hotmail.com>
2016-04-05 23:45:09 -0300

    Creation of class Food

---
+++ app/src/main/java/com/example/android/buddybuilding/Food.java
@@ -0,0 +1,43 @@
+package com.example.android.buddybuilding;
+
+/**
+ * Created by Alessandro on 05/04/2016.
+ */
+public class Food {
+    String name;
+    double protein;
+    double carbohydrates;
+    double calories;
+    double totalFat;
+
+    Food(String name, double protein, double carbohydrates, double totalFat)
+    {
+        this.name = name;
+        this.protein = protein;
+        this.carbohydrates = carbohydrates;
+        this.totalFat = totalFat;
+
+        calculateCalories();
+    }
+
+    private void calculateCalories()
+    {
+        calories = (9 * getTotalFat()) + (4 * getProtein()) + (4 * getCarbs());
+    }
+
+    public double getProtein()
+    {
+        return protein;
+    }
+
+    public double getCarbs()
+    {
+        return carbohydrates;
+    }
+
+    public double getTotalFat()
+    {
+        return totalFat;
+    }
+
+}
