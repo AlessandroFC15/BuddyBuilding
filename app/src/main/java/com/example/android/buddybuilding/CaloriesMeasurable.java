346fcefe17c6d5e8649eab48251c113b4eeff797
Alessandro <ale-remo@hotmail.com>
2016-04-18 14:30:27 -0300

    2 interface criadas

---
+++ app/src/main/java/com/example/android/buddybuilding/CaloriesMeasurable.java
@@ -0,0 +1,10 @@
+package com.example.android.buddybuilding;
+
+/**
+ * Created by Alessandro on 18/04/2016.
+ */
+public interface CaloriesMeasurable {
+    public int getCalories();
+
+    public void calculateCalories();
+}
---
+++ app/src/main/java/com/example/android/buddybuilding/PhysicallyActive.java
@@ -0,0 +1,14 @@
+package com.example.android.buddybuilding;
+
+public interface PhysicallyActive {
+
+    // Constants
+    public static final int NOT_VERY_ACTIVE = 0;
+    public static final int LIGHTLY_ACTIVE = 1;
+    public static final int ACTIVE = 2;
+    public static final int VERY_ACTIVE = 3;
+
+    public void setActivityLevel(int choice);
+
+    public int getActivityLevel();
+}
